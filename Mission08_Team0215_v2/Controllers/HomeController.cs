2c4e015b95dee32132afa95b38599f1da66c466d
tchappy15 <153124713+tchappy15@users.noreply.github.com>
2025-02-28 00:48:42 -0700

    fixed Controller comments

--- Mission08_Team0215_v2/Controllers/HomeController.cs
+++ Mission08_Team0215_v2/Controllers/HomeController.cs
@@ -14,35 +14,12 @@ public class HomeController : Controller
         _repo = temp; //so we can access the repo instead of the actual context file itself
     }
 
-    /// ###################################### finish:
     // organization:
     // Index Get and Post
-    // add task get and post
+    // Add task get and post
     // Edit get and post
-    // delete get and post
+    // Delete get and post
 
-    //[HttpGet]
-    //public IActionResult Index()
-    //{
-    //    var quadrants = _repo.Quadrants //go to the context file (which is like the C# version of our database), go to the Quadrants table, then use "linq"
-    //            .Include(x => x.Category) //a way of joining to the Cat table
-    //            .Where(x => x.Completed == false) //only grab tasks that have not been completed
-    //            .OrderBy(x => x.Quadrant).ToList(); //this List is what we send to the view
-
-
-    //    return View(quadrants); //by default, returns the Index view, and then also the quadrants since we specified that
-    //}
-
-    //[HttpPost]
-    //public IActionResult Index(Quadrant q)
-    //{
-    //    if (ModelState.IsValid)
-    //    {
-    //        _repo.
-    //        _repo.AddQuadrant(q);
-    //    }
-    //    return View(new Quadrant());
-    //}
 
     [HttpGet]
     public IActionResult Index()
@@ -70,39 +47,6 @@ public class HomeController : Controller
 
 
 
-    ////AddTask get
-    //[HttpGet]
-    //public IActionResult AddTask()
-    //{
-    //    ViewBag.Categories = _repo.Categories
-    //        .OrderBy(x => x.CategoryName)
-    //        .ToList();
-
-    //    return View("AddTask", new Quadrant());
-    //}
-
-    ////AddTask post:
-    //[HttpPost]
-    //public IActionResult AddTask(Quadrant response)
-    //{
-
-    //    if (ModelState.IsValid)
-    //    {
-    //        _repo.AddQuadrant(response); ; //add record to the database and save or commit the changes 
-
-    //        return View("Confirmation", response); //take user to confirmation page
-    //    }
-    //    else //invalid data was attempted by the user
-    //    {
-    //        ViewBag.Categories = _repo.Categories
-    //        .OrderBy(x => x.CategoryName)
-    //        .ToList();
-
-    //        return View(response); //pass them back the data they have already put in
-    //    }
-
-    //}
-
     // AddTask (GET)
     [HttpGet]
     public IActionResult AddTask()
@@ -111,7 +55,7 @@ public class HomeController : Controller
             .OrderBy(x => x.CategoryName)
             .ToList();
 
-        return View(new Quadrant()); // No need to specify "AddTask" since it matches the method name
+        return View(new Quadrant()); 
     }
 
     // AddTask (POST)
@@ -134,20 +78,6 @@ public class HomeController : Controller
     }
 
 
-
-    //Edit Get
-    //[HttpGet]
-    //public IActionResult Edit(int id) //This int HAS to be named "id" since that is what the pattern needs
-    //{
-    //    var recordToEdit = _repo.Quadrants
-    //        .Single(x => x.TaskId == id); //use .Single instead of .Where so that we only are returned one record only
-
-    //    ViewBag.Categories = _repo.Categories
-    //        .OrderBy(x => x.CategoryName)
-    //        .ToList();
-
-    //    return View("AddTask", recordToEdit);
-    //}
 
     [HttpGet]
     public IActionResult Edit(int id)
@@ -174,15 +104,7 @@ public class HomeController : Controller
         if (ModelState.IsValid)
         {
             _repo.UpdateQuadrant(updatedRecord);
-            //_repo.Update(updatedRecord);
-            //_repo.SaveChanges(); //do we change these two lines to being in the IQuadrantsRepository file? Ex:
-            //public void UpdateManager(Manager manager)
-            //    {
-            //        _context.Update(manager);
-            //        _context.SaveChanges();
-            //    }
-
-
+           
 
             //IMPORTANT: this is calling a VIEW, NOT an action, so we do this instead:
             return RedirectToAction("Index");
@@ -197,26 +119,6 @@ public class HomeController : Controller
         }
     }
 
-
-    ////Delete Get
-    //[HttpGet]
-    //public IActionResult Delete(int id)
-    //{
-    //    var recordToDelete = _repo.Quadrants
-    //        .Single(x => x.TaskId == id);
-
-    //    return View(recordToDelete);
-    //}
-
-    ////Delete Post
-    //[HttpPost]
-    //public IActionResult Delete(Quadrant app)
-    //{
-    //    _repo.Quadrants.Remove(app);
-    //    _repo.SaveChanges();
-
-    //    return RedirectToAction("Index");
-    //}
 
     // Delete (GET) - Confirm before deleting
     [HttpGet]
@@ -233,18 +135,6 @@ public class HomeController : Controller
         return View(recordToDelete);
     }
 
-    // Delete (POST) - Actually delete the record
-    //[HttpPost]
-    //public IActionResult DeleteConfirmed(int id) 
-    //{
-    //    var recordToDelete = _repo.Quadrants
-    //        .SingleOrDefault(x => x.TaskId == id);
-
-    //        _repo.DeleteQuadrant(recordToDelete); // Use repository method
-
-
-    //    return RedirectToAction("Index"); // Redirect to main page
-    //}
 
     [HttpPost]
     public IActionResult Delete(Quadrant record)
