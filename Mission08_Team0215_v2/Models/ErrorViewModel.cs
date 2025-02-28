c3579a6354b1ad04a75c1cd76a850f221bdb1461
tchappy15 <153124713+tchappy15@users.noreply.github.com>
2025-02-27 22:47:34 -0700

    Add project files.

---
+++ Mission08_Team0215_v2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission08_Team0215_v2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
